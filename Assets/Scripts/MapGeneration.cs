86f9db9c28a517e5444afd42ddcb6ab76f1eab92
Jihyo Kim <kimjihyo0325@gmail.com>
2018-11-02 13:04:26 -0700

    initialized git

---
+++ Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneration : MonoBehaviour {
+
+	public int mapSize = 5;
+	public Material tileColor;
+
+	void Start() {
+		GenerateMap();
+	}
+
+	void GenerateMap() {
+		for (int y = 0; y < mapSize; y++) {
+			for (int x = 0; x < mapSize; x++) {
+				GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Quad);
+				tile.transform.position = new Vector3(x, y, -1);
+				tile.GetComponent<Renderer>().material = tileColor;
+			}
+		}
+	}
+}
